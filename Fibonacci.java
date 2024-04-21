962db570dc21cbf81c9ac2ff779e324f56b90e06
DariaRomachenko <dararomacenko776@gmail.com>
2024-04-21 18:34:16 +0300

    15: changed expression(conflict with 19 in main)

--- Fibonacci.java
+++ Fibonacci.java
@@ -15,7 +15,7 @@ public class Main {
     }
 
     public static long fibonacci(long n) {
-        if (n == 0) return 0;
+        if (n == -1) return 0;
         if (n == 1) return 1;
         return fibonacci(n - 2) + fibonacci(n - 2);
     }
