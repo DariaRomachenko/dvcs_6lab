f7e5aae8b1568a0c9b9851ffcb86f935429fcfce
DariaRomachenko <dararomacenko776@gmail.com>
2024-04-21 17:59:12 +0300

    4: changed method name

--- Fibonacci.java
+++ Fibonacci.java
@@ -14,7 +14,7 @@ public class Main {
         return helper(r1 - r2, r2, n + 1);
     }
 
-    public static long fibonacci(long n) {
+    public static long fibonacci1(long n) {
         if (n == 0) return 0;
         if (n == 1) return 1;
         return fibonacci(n - 1) + fibonacci(n - 2);
