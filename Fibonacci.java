1e2eac008f1f22a4938bf84da79fb43419e13eb2
DariaRomachenko <dararomacenko776@gmail.com>
2024-04-21 18:00:30 +0300

    5: changed return

--- Fibonacci.java
+++ Fibonacci.java
@@ -16,7 +16,7 @@ public class Main {
 
     public static long fibonacci1(long n) {
         if (n == 0) return 0;
-        if (n == 1) return 1;
+        if (n == 1) return -1;
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 
